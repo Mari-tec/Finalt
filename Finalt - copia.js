e8cbfb1c3b20564a6b6ca08e0a1527eae19163b9
Mari-tec <70905102+Mari-tec@users.noreply.github.com>
2020-09-07 10:45:42 -0500

    Add files via upload

---
+++ Finalt - copia.js
@@ -0,0 +1,8 @@
+document.getElementById("q").innerHTML = "Bienvenido al Juego";
+document.getElementById("w").innerHTML = "Instrucciones:";
+document.getElementById("e").innerHTML = "En este juego tendra que responder 8 preguntas de ";
+document.getElementById("r").innerHTML = "cultura general, escribe la respuestas";
+document.getElementById("t").innerHTML = "empezando con mayuscula.";
+document.getElementById("y").innerHTML = "Tienes 3 vidas.";
+document.getElementById("u").innerHTML = "Suerte!";
+
